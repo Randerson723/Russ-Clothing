d50f8b1735f041c290980717ebc77883648e95bf
Russell W Anderson <92950114+Randerson723@users.noreply.github.com>
2022-01-12 19:16:57 -0600

    update functionality with Router, adding navigation from homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Homepage from './views/homepage.component.jsx'
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/hats' component={HatsPage} />
+        
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default App
