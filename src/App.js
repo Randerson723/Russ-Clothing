5caa2967342ca0a12267ee295c8c626309500cfa
Russell W Anderson <92950114+Randerson723@users.noreply.github.com>
2022-01-12 20:40:05 -0600

    adding header to homepage

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Homepage from './views/homepage/homepage.component.jsx'
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './views/shop/shop.component.jsx';
+import Header from './components/header/header.component'
 
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
-        
       </Switch>
     </div>
   );
